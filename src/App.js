ad3aa2e6810c3cd2fb27875435bd667dc08f21c6
zthaver <zainthaver262@gmail.com>
2023-03-28 10:59:04 -0400

    started basic login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import {useState} from "react";
+
 function App() {
+  const [name,setName] = useState('');
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+
+  async function registerUser(event)
+  {
+
+
+
+    event.preventDefault();
+
+    const bodyContents = `{"name":"${name}","email":"${email}","password":"${password}"}`
+    console.log(bodyContents);
+
+    const requestBody = JSON.parse(bodyContents);
+    console.log(requestBody);
+    console.log(bodyContents);
+    const response = await fetch('/api/user/register',{
+      method:'POST',
+      body:requestBody,
+      headers:
+      {
+        'Content-type': 'application/json'
+      }
+    }).then( async(data)=>{
+      console.log(data);
+    }).catch((err)=>{
+      console.log(err);
+    })
+    console.log(response);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1> Register </h1>
+      <form onSubmit={registerUser}>
+          <input type="text" 
+          placeholder="Name"
+          value={name}  
+          onChange={(e)=>setName(e.target.value) }
+          />
+          <input type="text" 
+          placeholder="Email"
+          value={email}  
+          onChange={(e)=>setEmail(e.target.value) }
+          />
+          <input type="password" 
+          placeholder="Password"
+          value={password} 
+          onChange={(e)=>setPassword(e.target.value) }
+          />
+          <input type="submit" value="Register"/>
+      </form>
     </div>
   );
 }
